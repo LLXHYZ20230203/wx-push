61c92facacb9587e31f0e09cd76dc59edd4b419f
LLXHYZ <112771364+LLXHYZ20230203@users.noreply.github.com>
2023-02-28 13:50:28 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
-  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
+  appid: "wxb0c738539bf82e1c",
+  appsecret: "e2d7761d59ce2a7a20cb4eed5a792b03",
+  template_id: "DJQ4FruhiZsGycSWQko0bVqatxvtcpaSMak3j3T7K0g",
+  touser: "o8Yn15ukWDHfxzuLSVTkgQmmSQ18",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
-  first_date: "2022-11-10",
+  city: "栾川县",
+  first_date: "2023-02-03",
 };
